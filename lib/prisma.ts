878333cc827241b9969503606b764557117f65ab
Renzo <renzocainaedge@gmail.com>
2025-06-28 15:43:00 -0600

    update prisma to work with neon in pord

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,7 +4,16 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({
+    datasources: {
+      db: {
+        url:
+          process.env.NODE_ENV === 'production'
+            ? process.env.DATABASE_URL_POOLING
+            : process.env.DATABASE_URL,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
