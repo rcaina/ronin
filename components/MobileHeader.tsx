f63a2285a4d44c211dfb355b2dba8351b7ef6d48
Renzo <renzocainaedge@gmail.com>
2025-07-17 22:06:29 -0600

    clean up

--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Menu, X, User, LogOut } from "lucide-react";
+import { Menu, X, LogOut } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
 
 interface NavItem {
